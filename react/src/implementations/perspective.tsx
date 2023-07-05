701c99f053e27ae5f80ed2ec87f08cfdaf0d35a4
John Hall <jhall@flexgen.com>
2023-07-05 11:02:13 -0400

    added perspective, not working

--- react/src/implementations/perspective.tsx
+++ react/src/implementations/perspective.tsx
@@ -1,12 +1,21 @@
 import { LibraryImplementation } from "../pages/Base";
 // import stackedBarData from "../data/stackedBar.json";
 // import line1Data from "../data/line1.json";
-// import line2Data from "../data/line2.json";
+import line2Data from "../data/line2.json";
 // import heatmapData from "../data/heatmap.json";
 // import sankeyData from "../data/sankey.json";
 // import contourData from "../data/contour.json";
 
-const template: LibraryImplementation = {
+import perspective from "@finos/perspective";
+import "@finos/perspective-viewer";
+import "@finos/perspective-viewer-datagrid";
+import "@finos/perspective-viewer-d3fc";
+import "@finos/perspective-viewer/dist/umd/all-themes.css";
+import { useEffect, useRef } from "react";
+
+const worker = perspective.shared_worker();
+
+const Perspective: LibraryImplementation = {
   documentation: {
     review: {
       rating: 0,
@@ -22,7 +31,26 @@ const template: LibraryImplementation = {
     return <p>not yet implemented</p>;
   },
   getLine2: function (): JSX.Element {
-    return <p>not yet implemented</p>;
+    const viewerRef = useRef();
+
+    useEffect(() => {
+      let ignore = false;
+      const load = async () => {
+        if (ignore) return;
+
+        const table = await worker.table(line2Data);
+
+        await viewerRef.current.load(table);
+      };
+
+      load();
+
+      return () => {
+        ignore = true;
+      };
+    }, []);
+
+    return <perspective-viewer ref={viewerRef} />;
   },
   getHeatmap: function (): JSX.Element {
     return <p>not yet implemented</p>;
@@ -38,4 +66,4 @@ const template: LibraryImplementation = {
   },
 };
 
-export default template;
+export default Perspective;
